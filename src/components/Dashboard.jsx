a8387bd7326e97cedb993c18d91f874e8363afb7
Vinay <vinay71625@gmail.com>
2024-08-06 22:35:47 +0530

    final-commit-1

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,6 +6,8 @@ import ProgressBar from "./ProgressBar";
 import Chart from "./Chart";
 import Tables from "./Tables";
 import Goals from "./Goals";
+import Feedback from "./Feedback";
+import { Card } from "./ui/card";
 
 const Dashboard = ({ sideBarToggle, setSideBarToggle }) => {
   return (
@@ -43,7 +45,26 @@ const Dashboard = ({ sideBarToggle, setSideBarToggle }) => {
             <div>
               <Goals />
             </div>
-            <div>feedback</div>
+            <div>
+              <p className="font-extrabold text-3xl">Feedback</p>
+              <Card>
+                <Feedback
+                  name={"Krishna"}
+                  stars={4}
+                  message={"Nice food, Could have been better"}
+                />
+                <Feedback
+                  name={"Vipin"}
+                  stars={3.5}
+                  message={"Rude owner, average food no timely service."}
+                />
+                <Feedback
+                  name={"Sam"}
+                  stars={5}
+                  message={"Great Location, good food, good service."}
+                />
+              </Card>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,7 +1,44 @@
 import React from "react";
-
-const Feedback = () => {
-  return <div>Feedback</div>;
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { FaStar } from "react-icons/fa";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+const Feedback = ({ name, stars, message }) => {
+  return (
+    <Card className="bg-blue-950">
+      <CardHeader>
+        <CardTitle>
+          <div className="flex flex-row space-x-3">
+            <div>
+              <Avatar>
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </div>
+            <div className="font-bold text-2xl text-slate-400">{name}</div>
+          </div>
+        </CardTitle>
+        <CardDescription>
+          {" "}
+          <div className="flex justify-center flex-row space-x-3 text-yellow-400">
+            <div>
+              <FaStar />
+            </div>
+            <div>{stars}</div>
+          </div>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <article className="text-balance text-slate-300">{message}</article>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Feedback;
